debb24e02295837099a7f310b46e99703d84b0c8
ANITH <95991675+Anith58@users.noreply.github.com>
2026-03-12 17:53:00 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
+
+module.exports = nextConfig;
